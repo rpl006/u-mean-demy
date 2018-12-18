9c84ce65d56e343d2b95e8799348f52d070b4482
DWproduction5 <dwproduction5@DWproduction5.local>
2018-12-18 08:36:13 -0600

    Created Post-list Component(initial settings)

--- mean-project-site/src/app/app.module.ts
+++ mean-project-site/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
